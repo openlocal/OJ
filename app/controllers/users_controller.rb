81339f7a9a0dbc11f06e5918d37d8f5729d416cf
Pat Allan <pat@freelancing-gods.com>
2010-03-07 02:24:58 +1100

    Allowing users' profiles to be viewed publicly.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < Clearance::UsersController
   def show
-    redirect_to root_path if params[:id].to_i != current_user.id
-    @user = current_user
+    @user = User.find params[:id]
   end
   
   def update
